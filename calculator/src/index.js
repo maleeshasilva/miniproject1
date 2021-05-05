e309355d6c5c01431ff9cd916789ed33d5823030
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-05-05 11:20:30 +0530

    component part 1

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -1,8 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './index.css';
 
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+var calculatePayment =function(principal,years,rate){
+  var monthlyRate = rate / 100 / 12;
+  var monthyPayment = principal * monthlyRate / (1 -(Math.pow(1/(1+monthlyRate),years*12)));
+  var balance =principal;
+  for( var y=0;y<years;y++){
+    var interestY =0;
+    var principalY =0;
+    for(var m=0;m<12;m++){
+      var interestM = balance * maonthlyRate;
+      var principalM =monthyPayment -interestM;
+      interestY =interestY + interestM;
+      principalY =principalY +principalM;
+      balance = balance - principalM;
+    }
+  }
+ return {monthlyPayment: monthyPayment};
+};
+
+var Header = React.createClass({
+render:function(){
+  return(
+    <header>
+      <h1>{this.props.title}</h1>
+    </header>
+  )
+}
+
+});
+
+
 
 ReactDOM.render(
   <React.StrictMode>
