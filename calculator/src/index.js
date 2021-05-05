dc43c6816aa4e8c88f52c163961405e48476cc6b
maleeshasilva <maleeshadinolisilva@gmail.com>
2021-05-05 12:18:24 +0530

    component  part 2

--- calculator/src/index.js
+++ calculator/src/index.js
@@ -22,6 +22,60 @@ var calculatePayment =function(principal,years,rate){
  return {monthlyPayment: monthyPayment};
 };
 
+
+var MortgageCalculator =React.createClass({
+getInitialState:function(){
+
+  return{
+
+    principal:this.props.principal,
+    years :this.props.years,
+    rate :this.props.rate    
+  };
+},
+principalChange:function(event){
+  this.setState({principal:event.target.value});
+  },
+
+yearsChange:function(event){
+  this.setState({years:EventTarget.target.value});
+},
+rateChange:function(event){
+  this.setState({rate:event.target.value});
+},
+render:function(){
+  var payment = calculatePayment(this.state.principal,this.state.years,this.state.rate);
+  var monthlyPayment =payment.monthlyPayment;
+
+  return(
+    <div className ="Content">
+      <div className ="form">
+        <div>
+          <labele>Principal:</labele>
+          <input type ="text" value ={this.state.principal} onChange ={this.principalChange}/>
+          </div>
+      </div>
+
+      <div>
+        <label>Years:</label>
+        <input type ="text" value={this.state.principal} onChange ={this.principalChange}/>
+      </div>
+
+    <div>
+      <label htmlFor ="rate">Rate:</label>
+      <input type ="text" value ={this.state.rate} onChange={this.rateChange}/>
+    </div>
+
+    <h2>Monthly Payment: <span ClassName ="currency">{Number(monthlyPayment.toFixed(2)).toLocaleString()}</span></h2>
+          </div>
+  );
+
+}  
+
+});
+
+
+
 var Header = React.createClass({
 render:function(){
   return(
